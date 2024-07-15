54adf9fcf204b1c9f60113f1913f514199b992da
HP <HP@DESKTOP-O10K6DV>
2024-07-15 09:20:27 +0700

    Add project files.

---
+++ WebBookingHotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBookingHotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
